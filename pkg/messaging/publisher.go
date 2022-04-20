2d322d38ef0bce422561d9cd6770ee31c740eeb6
王浩天 <wang@WangHaotian.local>
2022-04-20 11:47:22 +0800

    add docker-compose

--- pkg/messaging/publisher.go
+++ pkg/messaging/publisher.go
@@ -1,1 +1,62 @@
 package messaging
+
+import (
+	"fmt"
+	"github.com/streadway/amqp"
+)
+
+type Publisher struct {
+	conn *amqp.Connection
+}
+
+/*
+NewPublisher 创建一个Publisher并且返回其指针
+*/
+func NewPublisher(user string, password string, host string, port string) (*Publisher, error) {
+	url := fmt.Sprintf("amqp://%s:%s@%s:%s/", user, password, host, port)
+	publisher := new(Publisher)
+	var err error
+	publisher.conn, err = amqp.Dial(url)
+	if err != nil {
+		return nil, err
+	}
+	return publisher, nil
+}
+
+/*
+Publish 向指定的交换机广播一条信息并立即返回，广播类型为FANOUT
+
+*/
+func (p *Publisher) Publish(exchangeName string, body []byte, contentType string) error {
+	ch, err := p.conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		exchangeName,
+		amqp.ExchangeFanout,
+		true,
+		false,
+		false,
+		false,
+		nil)
+	if err != nil {
+		return err
+	}
+
+	err = ch.Publish(
+		exchangeName,
+		exchangeName,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: contentType,
+			Body:        body,
+		})
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- pkg/messaging/subscriber.go
+++ pkg/messaging/subscriber.go
@@ -1,1 +1,101 @@
 package messaging
+
+import (
+	"fmt"
+	"github.com/streadway/amqp"
+)
+
+type HandleFunc func(d amqp.Delivery)
+
+type Subscriber struct {
+	conn *amqp.Connection
+}
+
+/*
+NewSubscriber 创建一个Subscriber并且返回其指针
+
+*/
+func NewSubscriber(user string, password string, host string, port string) (*Subscriber, error) {
+	url := fmt.Sprintf("amqp://%s:%s@%s:%s/", user, password, host, port)
+	subscriber := new(Subscriber)
+	var err error
+	subscriber.conn, err = amqp.Dial(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return subscriber, nil
+}
+
+/*
+Subscribe 从不退出，直到stopCh收到消息
+
+exchangeName 订阅的交换机名称
+
+handler 传入的处理函数
+
+*/
+func (s *Subscriber) Subscribe(exchangeName string, handler HandleFunc, stopCh <-chan struct{}) error {
+	ch, err := s.conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		exchangeName,
+		amqp.ExchangeFanout,
+		true,
+		false,
+		false,
+		false,
+		nil)
+	if err != nil {
+		return err
+	}
+
+	queue, err := ch.QueueDeclare(
+		"",
+		false,
+		true,
+		false,
+		false,
+		nil)
+	if err != nil {
+		return err
+	}
+
+	err = ch.QueueBind(
+		queue.Name,
+		exchangeName,
+		exchangeName,
+		false,
+		nil)
+	if err != nil {
+		return err
+	}
+
+	msgs, err := ch.Consume(
+		queue.Name, // queue
+		"",         // consumer
+		true,       // auto-ack
+		false,      // exclusive
+		false,      // no-local
+		false,      // no-wait
+		nil,        // args
+	)
+	if err != nil {
+		return err
+	}
+
+	go consumeLoop(msgs, handler)
+	<-stopCh
+	return nil
+}
+
+func consumeLoop(deliveries <-chan amqp.Delivery, handler HandleFunc) {
+	for d := range deliveries {
+		// Invoke the handlerFunc func we passed as parameter.
+		handler(d)
+	}
+}
