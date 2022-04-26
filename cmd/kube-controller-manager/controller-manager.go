c95f9673ce919fa5f6d795daf0bc3b09122b2786
QianBowen <925763182@qq.com>
2022-04-26 12:27:29 +0800

    feat: try new controller

--- cmd/kube-controller-manager/controller-manager.go
+++ cmd/kube-controller-manager/controller-manager.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"minik8s/cmd/kube-controller-manager/app"
 	"minik8s/pkg/klog"
 )
 
 func main() {
 	klog.Infof("running controller manager.go\n")
+	command := app.NewControllerManagerCommand()
+	err := command.Execute()
+	if err != nil {
+		klog.Errorf("controller command fail")
+	}
 }
